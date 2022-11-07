a738eef67308aa120df2c9b09469eefc2fb33320
Ric Evans <emejqz@gmail.com>
2022-11-07 13:36:09 -0600

    use David's suggested code

--- request_removal.py
+++ request_removal.py
@@ -29,18 +29,10 @@ def main() -> None:
     # read
     with open(DOCKER_IMAGES_FILE, "r") as f:
         lines = [ln.strip() for ln in f.readlines()]  # rm each trailing '\n'
-        matches = [  # constrict regex str to just one line
-            ln for ln in lines if re.match(rf"^.* {args.remove_regex_path}$", ln)
-        ]
-        # remove all matches from lines
-        lines = [ln for ln in lines if ln not in matches]
-        # if an image was negated, strip that & add, but only if it's unique (retain original order)
-        cvmfs_image_str = [s := m.lstrip("-") for m in matches if s not in matches]
 
-    # append negations of each
-    for img in cvmfs_image_str:
-        lines.append(f"-{img}")
-        logging.debug(f"Added line to {DOCKER_IMAGES_FILE}: {lines[-1]}")
+    matcher = re.compile(rf"^[^-].+ {args.remove_regex_path}$")  # compile once
+    # negate any matched lines, keep the rest
+    lines = [f"-{ln}" if matcher.match(ln) else ln for ln in lines]
 
     # write
     with open(DOCKER_IMAGES_FILE, "w") as f:
