1750386db6e695c74c96f5dc85460cb6c57ecdb3
ric-evans <emejqz@gmail.com>
2025-02-24 19:26:46 -0600

    comments

--- request_removal.py
+++ request_removal.py
@@ -11,13 +11,23 @@ SHA_TOKEN = "[SHA]"
 
 
 def _matches_pattern(image_pattern: str, cvmfs_image: str) -> bool:
+    """Determine if a CVMFS image matches the given pattern w/ known tokens."""
+
     # [SHA] suffix
     if image_pattern.endswith(SHA_TOKEN):
-        base = image_pattern[: -len(SHA_TOKEN)]  # w/o SHA_TOKEN suffix
+        # Example Matches:
+        # "feature-branch-[SHA]" -> Matches "feature-branch-abc123"
+        # "feature-branch-[SHA]" -> Does NOT match "feature-branch-xyz-abc123"
+
+        # w/o SHA_TOKEN suffix (ex: feature-branch-)
+        base = image_pattern[: -len(SHA_TOKEN)]
         if not cvmfs_image.startswith(base):
             return False
-        potential_sha = cvmfs_image[len(base) :]  # the suffix of an actual image
+
+        # the suffix of an actual image (ex: abc123 -> True; xyz-abc123 -> False)
+        potential_sha = cvmfs_image[len(base) :]
         return bool(SHA_PATTERN.fullmatch(potential_sha))
+
     # FUTURE DEV: support additional string tokens
     # Exact match case
     else:
